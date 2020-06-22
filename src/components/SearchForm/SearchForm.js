7e1c30e794036f9ad24cc4d116bc4e79401c2ecd
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-06-22 11:26:37 -0600

    Update form structure and items in state

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 import './SearchForm.css';
 
 class SearchForm extends Component {
+    state = {
+        username: '',
+        location: ''
+    }
+
+    queryUsers = e => {
+        e.preventDefault()
+
+    }
     render () {
         return (
-            <form></form>
+            <form>
+                <input 
+                type='text'
+                name='username'
+                placeholder='Username'
+                value={this.state.username}
+                />
+                <input 
+                type='text'
+                name='location'
+                placeholder='Location'
+                value={this.state.location}
+                />
+                <button
+                onClick={this.queryUsers} 
+                > Submit
+                </button>
+            </form>
         )
     }
 } 
